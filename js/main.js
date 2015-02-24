d67592eeabcfa775a7c6479d2d8508da4c7199af
Condition17 <C.conache@gmail.com>
2015-02-24 21:30:04 +0200

    Global-warming

--- js/main.js
+++ js/main.js
@@ -1,23 +1,40 @@
 var mn = function( ){
 
-$("nav").fadeTo("slow",0.5); //on load...
+
+	var init=parseFloat($(".navbar-brand").css("font-size"));
+	var size2=init+4;
+	
+	var bar_init=parseFloat($("nav").css("height"));
+	var bar_transform=bar_init+2;
+	
+$("nav").fadeTo("normal",0.5);
 	
 $("nav").hover(function()
 			   {
 	$("nav").fadeTo("fast",1); // hovering the navbar...
 	
 	
-}
-,
+	
+	$(".navbar-brand").animate({fontSize:size2}, "normal");
+
+	
+	
+},
+
 function(){
 	
+
+		$("nav").fadeTo("normal",0.5);
+
 	
-		$("nav").fadeTo("normal",0.5); //after hover;
-	
+	$(".navbar-brand").animate({fontSize:init}, "normal");
 }
 			  );
 	
 	
+$("a").hover(function(){
+	$(this).toggleClass("check");
+});
 
 
 }
